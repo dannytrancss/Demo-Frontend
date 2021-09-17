ec7ca9d7dc07921f75eca06f3fa9cd4c76bf4336
dannyvop <thanhsuol@gmail.com>
2021-09-17 12:24:30 +0800

    Update home.js
    Adding  some comments

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import AgentForm from '../components/home/agent';
-//import DemoTable from '../components/home/demo';
-import AgentTable from "../components/home/table";
+import AgentForm from '../components/home/agent_form';
+import AgentTable from "../components/home/agent_table";
 
 function AppHome() {
   return (
     <div className="main">
       <AgentForm></AgentForm>
-      {/* <DemoTable/> */}
       <AgentTable></AgentTable>
     </div>
   );
